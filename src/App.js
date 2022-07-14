3ed5c6b3ac8130fd0b3695013442ea66acd35b07
jamiee0411 <itsjamie0411@gmail.com>
2022-07-14 15:55:14 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'; 
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { cats } from './cats';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cats: cats, 
+      searchfield: ''
+    }
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+    }
+
+  render() {
+    const filteredCats = this.state.cats.filter(cats => {
+      return cats.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+      <div className='tc'>
+        <h1 className='f1'>CatFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList cats={ filteredCats }/>
+      </div>
+    );  
+  }
+}
+
+export default App; 
