ad8f126e171c0ce5eeba4323bda0abc3f82eb08d
jamiee0411 <itsjamie0411@gmail.com>
2022-07-27 07:49:02 -0700

    removing throw error

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'; 
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
+import ErrorBounry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -34,9 +34,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>CatFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll> 
-            <CardList cats={ filteredCats }/>
-          </Scroll>
+            <ErrorBounry>
+              <CardList cats={ filteredCats }/>
+            </ErrorBounry>
         </div>
       );  
     }
